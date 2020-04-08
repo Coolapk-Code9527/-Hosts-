c02c89f79adf50d83d3eac0d0efec323e5a615b2
㐅代号:9527 <45525896+Coolapk-Code9527@users.noreply.github.com>
2020-04-08 19:46:38 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -7,6 +7,9 @@ MODDIR=${0%/*}
 
 # 该脚本将在late_start服务模式下执行
 sleep 20
+# IP转发,伪装
+#sysctl -w net.ipv4.ip_forward=1
+#sysctl -w net.ipv4.ip_dynaddr=1
 # IPTABLES 规则
 # IP(添加多个IP用,隔开)
 IP=119.39.80.248,58.251.150.40,119.39.80.43,119.39.80.42,58.251.150.31,119.39.120.64,58.251.150.37,119.39.80.56,36.250.8.220
@@ -17,49 +20,60 @@ IP=119.39.80.248,58.251.150.40,119.39.80.43,119.39.80.42,58.251.150.31,119.39.12
 # 域名拦截
 #iptables -I OUTPUT -d mss0.bdstatic.com -j DROP
 # 域名匹配拦截
-#iptables -I OUTPUT -m string --string ad --algo bm --to 65535 -j DROP
+#iptables -I OUTPUT -m string --string ad. --algo bm --to 65535 -j DROP
 # 禁止本地回环连接
 #iptables -A INPUT -i lo -j DROP
 #iptables -A OUTPUT -o lo -j DROP
-# 禁止ICMP ping
-#iptables -A INPUT -p icmp -j DROP
-#iptables -A OUTPUT -p icmp -j DROP
-# 端口重定向
-iptables -t nat -A PREROUTING -p tcp --dport 5353 -j REDIRECT --to-port 53
-iptables -t nat -A PREROUTING -p udp --dport 5353 -j REDIRECT --to-port 53
-# DNS目标地址重定向
-iptables -t nat -I PREROUTING -p tcp --dport 53 -j DNAT --to-destination 223.5.5.5:53
-iptables -t nat -I PREROUTING -p udp --dport 53 -j DNAT --to-destination 223.5.5.5:53
-iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 223.5.5.5:53
-iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 223.5.5.5:53
-# 动态IP地址伪装
+# 禁止ICMP Ping
+#iptables -A INPUT -p 1 -j DROP
+#iptables -A OUTPUT -p 1 -j DROP
+# 端口匹配重定向
+#iptables -t nat -A PREROUTING -p 6 -m multiport ! --dport 0:49151 -j REDIRECT --to-ports 53
+#iptables -t nat -A PREROUTING -p 17 -m multiport ! --dport 0:49151 -j REDIRECT --to-ports 53
+# DNS目标地址转换
+iptables -t nat -A PREROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A PREROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A OUTPUT ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A OUTPUT ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
 iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A POSTROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 -m multiport --sport 53,5353 -j SNAT --to-source 223.5.5.5:53
+iptables -t nat -A POSTROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 -m multiport --sport 53,5353 -j SNAT --to-source 223.5.5.5:53
+# 数据包检测匹配放行
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # 丢弃碎片数据包
 iptables -A INPUT -f -j DROP
 # 丢弃TCP空值数据包
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p 6 --tcp-flags ALL NONE -j DROP
 # 丢弃TCP标记为SYN但不是新创建的数据包
-iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+iptables -A INPUT -p 6 ! --syn -m state --state NEW -j DROP
 # 丢弃TCP标记为SYN/ACK但是新创建的数据包
-iptables -A INPUT -p tcp --tcp-flags SYN,ACK SYN,ACK -m state --state NEW -j DROP
+iptables -A INPUT -p 6 --tcp-flags SYN,ACK SYN,ACK -m state --state NEW -j DROP
 # 丢弃TCP标记组合异常的数据包
-iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
-iptables -A INPUT -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
-iptables -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
-iptables -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,ACK FIN -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags PSH,ACK PSH -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags ACK,URG URG -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,RST FIN,RST -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags SYN,RST SYN,RST -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,PSH,URG -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,SYN,RST,PSH,ACK,URG -j DROP
+iptables -A INPUT -p 6 --tcp-flags ALL ALL -j DROP
+iptables -A INPUT -p 6 --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables -A INPUT -p 6 --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+iptables -A INPUT -p 6 --tcp-flags ALL FIN,URG,PSH -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,ACK FIN -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags PSH,ACK PSH -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags ACK,URG URG -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,PSH,URG -j DROP
+iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,SYN,RST,PSH,ACK,URG -j DROP
 # 丢弃无效数据包
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A OUTPUT -m state --state INVALID -j DROP
 iptables -A FORWARD -m state --state INVALID -j DROP
+
+
+
+
+
+
+
 
 
 
