a30855837c0f9f10c686f444ea8dee2c63ff9d1c
㐅代号:9527 <45525896+Coolapk-Code9527@users.noreply.github.com>
2020-04-16 00:34:46 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -16,60 +16,47 @@ IP=119.39.80.248,58.251.150.40,119.39.80.43,119.39.80.42,58.251.150.31,119.39.12
 #iptables -I INPUT -s $IP -j DROP
 # IP地址重定向
 #iptables -t nat -I OUTPUT -d $IP -j DNAT --to-destination 120.0.0.1
-# 域名拦截
-#iptables -I OUTPUT -d mss0.bdstatic.com -j DROP
 # 域名匹配拦截
-#iptables -I OUTPUT -m string --string ad. --algo bm --to 65535 -j DROP
+#iptables -I OUTPUT -m string --string pgdt.gtimg.cn --algo bm --to 65535 -j DROP
 # 禁止本地回环连接
 #iptables -A INPUT -i lo -j DROP
 #iptables -A OUTPUT -o lo -j DROP
 # 禁止ICMP Ping
 #iptables -A INPUT -p 1 -j DROP
 #iptables -A OUTPUT -p 1 -j DROP
+# 数据包检测匹配放行
+iptables -t mangle -A PREROUTING -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -t mangle -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -t mangle -A FORWARD -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 # 端口匹配放行
-iptables -t mangle -A INPUT -p 6 -m multiport --dport 53,5353 -j ACCEPT
-iptables -t mangle -A INPUT -p 17 -m multiport --dport 53,5353 -j ACCEPT
 iptables -t mangle -A PREROUTING -p 6 -m multiport --dport 53,5353 -j ACCEPT
 iptables -t mangle -A PREROUTING -p 17 -m multiport --dport 53,5353 -j ACCEPT
 iptables -t mangle -A OUTPUT -p 6 -m multiport --dport 53,5353 -j ACCEPT
 iptables -t mangle -A OUTPUT -p 17 -m multiport --dport 53,5353 -j ACCEPT
 iptables -t mangle -A FORWARD -p 6 -m multiport --dport 53,5353 -j ACCEPT
 iptables -t mangle -A FORWARD -p 17 -m multiport --dport 53,5353 -j ACCEPT
-iptables -t mangle -A PREROUTING -p 6 -m multiport --dport 53,5353 -j ACCEPT
-iptables -t mangle -A PREROUTING -p 17 -m multiport --dport 53,5353 -j ACCEPT
 # 端口匹配标记
-iptables -t mangle -A INPUT -p 6 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A INPUT -p 17 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A PREROUTING -p 6 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A PREROUTING -p 17 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A OUTPUT -p 6 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A OUTPUT -p 17 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A FORWARD -p 6 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A FORWARD -p 17 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A POSTROUTING -p 6 -m multiport --dport 53,5353 -j MARK --set-mark 9527
-iptables -t mangle -A POSTROUTING -p 17 -m multiport --dport 53,5353 -j MARK --set-mark 9527
+iptables -t mangle -A PREROUTING -p 17 -m multiport --dport 53,5353 -j TPROXY --on-port 53 --tproxy-mark 0x2537
+iptables -t mangle -A INPUT -p 17 --sport 53 --dport 53 -j MARK --set-mark 9527
+iptables -t mangle -A OUTPUT -p 17 --sport 53 --dport 53 -j MARK --set-mark 9527
+iptables -t mangle -A FORWARD -p 17 --sport 53 --dport 53 -j MARK --set-mark 9527
+iptables -t mangle -A POSTROUTING -p 17 --sport 53 --dport 53 -j MARK --set-mark 9527
 # 建立路由策略
 ip route flush table 1
 ip route add local default dev lo table 1
 ip rule add fwmark 9527 table 1 prio 1 lookup 1
-#ip rule add from 223.5.5.5 prio 1 lookup 1
-#ip rule add to 223.5.5.5 prio 1 lookup 1
 ip route flush cache
-# 端口匹配重定向
-#iptables -t nat -A PREROUTING -p 6 -m multiport ! --dport 0:49151 -j REDIRECT --to-ports 53
-#iptables -t nat -A PREROUTING -p 17 -m multiport ! --dport 0:49151 -j REDIRECT --to-ports 53
+# 端口匹配转换
+iptables -t nat -A PREROUTING -p 6 -m multiport --dport 53,5353 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -p 17 -m multiport --dport 53,5353 -j REDIRECT --to-ports 53
 # DNS目标地址转换
-iptables -t nat -A PREROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
-iptables -t nat -A PREROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
-iptables -t nat -A OUTPUT ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
-iptables -t nat -A OUTPUT ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 -m multiport --dport 53,5353 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A PREROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 --dport 53 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A PREROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 --dport 53 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A OUTPUT ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 --dport 53 -j DNAT --to-destination 223.5.5.5:53
+iptables -t nat -A OUTPUT ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 --dport 53 -j DNAT --to-destination 223.5.5.5:53
 iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -t nat -A POSTROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 -m multiport --sport 53,5353 -j SNAT --to-source 223.5.5.5:53
-iptables -t nat -A POSTROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 -m multiport --sport 53,5353 -j SNAT --to-source 223.5.5.5:53
-# 数据包检测匹配放行
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -m state --state ESTABLISHED,NEW,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 6 --sport 53 -j SNAT --to-source 223.5.5.5:53
+iptables -t nat -A POSTROUTING ! -s 223.5.5.5 ! -d 223.5.5.5 -p 17 --sport 53 -j SNAT --to-source 223.5.5.5:53
 # 丢弃碎片数据包
 iptables -A INPUT -f -j DROP
 # 丢弃TCP空值数据包
@@ -96,4 +83,17 @@ iptables -A INPUT -p 6 -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,SYN,RST,PS
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A OUTPUT -m state --state INVALID -j DROP
 iptables -A FORWARD -m state --state INVALID -j DROP
+
+
+
+
+
+
+
+
+
+
+
+
+
 
